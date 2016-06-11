52e265355862208fa243e62a6c88a2c88d944d71
jieaido <jieaido@163.com>
2016-06-11 23:09:51 +0800

    更新了读取excel功能

--- BJForLYW/DB/ExcelHelper.cs
+++ BJForLYW/DB/ExcelHelper.cs
@@ -9,8 +9,9 @@ namespace BJForLYW.DB
 {
     public static  class ExcelHelper
     {
-        public static void GetPartFromExcel(string filePath)
+        public static List<Part> GetPartFromExcel(string filePath)
         {
+            List<Part> parts = new List<Part>();
             HSSFWorkbook hssfWorkbook;
             using (FileStream fileStream=new FileStream(filePath,FileMode.Open,FileAccess.Read))
             {
@@ -18,8 +19,39 @@ namespace BJForLYW.DB
             }
             var sheet= hssfWorkbook.GetSheetAt(0);
             var rows = sheet.GetRowEnumerator();
+            rows.MoveNext();
+            while (rows.MoveNext())
+            {
+                HSSFRow row= (HSSFRow) rows.Current;
+                Part part = new Part();
+                if (row.FirstCellNum == 0)
+                {
+                    part.PartNum = row.Cells[0].ToString();
+                    part.PartName = row.Cells[1].ToString();
+                    part.PartType = row.Cells[2].ToString();
+                    part.Unit = row.Cells[3].ToString();
+                    part.Price = (decimal?) row.Cells[4].NumericCellValue;
+                    part.Num = (long) row.Cells[5].NumericCellValue;
+                }
+                else
+                {
+                    part.PartNum = "";
+                    part.PartName = row.Cells[0].ToString();
+                    part.PartType = row.Cells[1].ToString();
+                    part.Unit = row.Cells[2].ToString();
+                    part.Price = (decimal?)row.Cells[3].NumericCellValue;
+                    part.Num = (long)row.Cells[4].NumericCellValue;
+                }
+             
+              
+                parts.Add(part);
+
+
+
+
+            }
             
-            return ;
+            return parts;
         }
               
     }
