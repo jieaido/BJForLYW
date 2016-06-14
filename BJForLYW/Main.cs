8ce597a1b6165652cc41d26fd8524212ef53747c
Jieaido-ThinkPad_T460P <Jieaido@163.com>
2016-06-14 22:31:31 +0800

    更新了设备表的搜索栏,搜索实时显示结果

--- BJForLYW/Main.cs
+++ BJForLYW/Main.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.Entity;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using BJForLYW.DB;
 
@@ -13,8 +9,9 @@ namespace BJForLYW
 {
     public partial class Main : Form
     {
-        PartContext pc=new PartContext();
         private IEnumerable<Part> allpartlist;
+        private readonly PartContext pc = new PartContext();
+
         public Main()
         {
             InitializeComponent();
@@ -24,62 +21,24 @@ namespace BJForLYW
         {
             pc.Parts.Load();
             partbindingSource1.DataSource = pc.Parts.Local.ToBindingList();
-           
-           
-           bindingNavigator1.BindingSource = partbindingSource1;
-           
-            
 
+
+            bindingNavigator1.BindingSource = partbindingSource1;
         }
 
         private void 保存SToolStripButton_Click(object sender, EventArgs e)
         {
-            
             pc.SaveChanges();
-
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+       
 
         private void button2_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 var filename = openFileDialog1.FileName;
-                var getPartlist= ExcelHelper.GetPartFromExcel(filename);
+                var getPartlist = ExcelHelper.GetPartFromExcel(filename);
                 pc.GetParts.AddRange(getPartlist);
                 GetPartBindingSource.DataSource = pc.GetParts.Local.ToBindingList();
                 dataGridView2.AutoGenerateColumns = true;
@@ -88,16 +47,13 @@ namespace BJForLYW
             }
         }
 
-       
 
         private void bindingNavigator2_RefreshItems(object sender, EventArgs e)
         {
-
         }
 
         private void splitContainer3_Panel2_Paint(object sender, PaintEventArgs e)
         {
-
         }
 
         private void 保存SToolStripButton1_Click_1(object sender, EventArgs e)
@@ -109,28 +65,54 @@ namespace BJForLYW
 
         private void comboBox1_TextUpdate(object sender, EventArgs e)
         {
-            
-            
-            
         }
 
         private void comboBox1_DropDown(object sender, EventArgs e)
         {
-            comboBox1.Items.Clear();
-            string searchTxt = comboBox1.Text.Trim();
-            var searchResult = pc.Parts.Where(
-                s => s.PartName.Contains(searchTxt) || s.PartNum.Contains(searchTxt) || s.PartType.Contains(searchTxt))
-                .Distinct();
-            comboBox1.BeginUpdate();
+        }
 
-            foreach (var part in searchResult)
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            var searchTxt = comboBox1.Text.Trim();
+            if (string.IsNullOrEmpty(searchTxt))
             {
-                comboBox1.Items.Add(part);
-
+                dataGridView1.DataSource = partbindingSource1;
             }
-            comboBox1.DisplayMember = "PartName";
-            comboBox1.ValueMember = "partid";
-            comboBox1.EndUpdate();
+            else
+            {
+                var searchResult = pc.Parts.Where(
+               s => s.PartName.Contains(searchTxt) || s.PartNum.Contains(searchTxt) || s.PartType.Contains(searchTxt))
+               .Distinct();
+                // partbindingSource1.DataSource = searchResult.ToList();
+                // bindingNavigator1.BindingSource = partbindingSource1;
+                dataGridView1.DataSource = searchResult.ToList();//
+                //todo 直接更改dataview的绑定值,数据量不变,更改bingsource的,数据量都变了
+                // dataGridView1.ResetBindings();
+            }
+            dataGridView1.ResetBindings();
+            //comboBox1.BeginUpdate();
+
+            //foreach (var part in searchResult)
+            //{
+            //    comboBox1.Items.Add(part);
+
+            //}
+            //comboBox1.DisplayMember = "PartName";
+            //comboBox1.ValueMember = "partid";
+            //comboBox1.EndUpdate();
+        }
+
+        private void comboBox1_Leave(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+
+        private void PartbindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
         }
     }
 }
